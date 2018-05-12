07b031da55efc10f5d895c86bc858072c50c12f0
Luis Bastidas <luisbastir@gmail.com>
2018-05-12 13:43:33 -0600

    sign in, login, authentication, session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show]
+  before_action :require_same_user, only: [:edit, :update]
   
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page], per_page: 5)
   end
 
   # GET /signup
@@ -12,9 +13,10 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  # GET /signup/1
-  # GET /signup/1.json
+  # GET /users/1
+  # GET /users/1.json
   def show
+    @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
   # GET /users/1/edit
@@ -26,8 +28,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to the alpha blog #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render "new"
     end
@@ -53,5 +56,12 @@ class UsersController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(:username, :email, :password)
+    end
+
+    def require_same_user
+      if current_user != @user
+        flash[:danger] = "You can only edit your own account"
+        redirect_to root_path
+      end
     end
 end
