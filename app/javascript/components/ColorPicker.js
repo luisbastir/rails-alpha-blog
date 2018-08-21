5cfdbe5bb41e88b8c9c988ae1b573ef31d433d18
Luis Bastidas <luisbastir@gmail.com>
2018-08-21 15:48:15 -0500

    ColorPicker Component Added in user edit view

--- app/javascript/components/ColorPicker.js
+++ app/javascript/components/ColorPicker.js
@@ -4,15 +4,28 @@ import PropTypes from "prop-types"
 import { SketchPicker } from "react-color"
 
 class ColorPicker extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { selector: props.selector };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = (color) => {
+    document.body.style.background = color.hex;
+    let elem = document.querySelector('#'+this.state.selector);
+    elem.value = color.hex;
+  }
+
   render() {
     return (
       <React.Fragment>
-        <SketchPicker />
+        <SketchPicker color={this.props.color} onChange={this.handleChange} />
       </React.Fragment>
     );
   }
 }
 
-ColorPicker.propTypes = {};
-
+ColorPicker.propTypes = {
+  color: PropTypes.string
+};
 export default ColorPicker
